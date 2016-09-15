cf0660a9126aaecba5275ba74ccc64f8b86bcf8a
Waqid <wmv214@nyu.edu>
2016-09-15 01:11:00 -0400

    Initialze

---
+++ app.js
@@ -0,0 +1,114 @@
+var express = require('express');
+var cors = require("cors");
+var bodyParser = require("body-parser");
+var DBConnection = require("./lib/DBConnection");
+var app = express();
+
+var weekdays = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+
+app.use(function(req, res, next) {
+    // console.log(`${req.method} request for '${req.url}' - ${JSON.stringify(req.body)}`);
+    next();
+});
+
+
+app.use(express.static("./public"));
+app.use('/jquery', express.static(__dirname + '/node_modules/jquery/dist/'));
+app.use('/bootstrap', express.static(__dirname + '/node_modules/bootstrap/dist/'));
+
+
+app.get("/api/owner/:id", function(req, res) {
+
+    var connection = DBConnection();
+
+    connection.connect();
+
+    var query = `SELECT * FROM owner O,business B
+					WHERE O.id=${req.params.id} AND O.id = B.owner_id`;
+
+
+    connection.query(query, function(err, rows, fields) {
+        if (!err) {
+            res.json(rows);
+        } else {
+            console.log(err)
+            res.json(err)
+        }
+    });
+
+    connection.end();
+
+});
+
+app.post("/api/notify", function(req, res) {
+
+    var connection = DBConnection();
+
+    connection.connect();
+
+    var query = `SELECT * FROM business_customer BC,customer C
+				WHERE  BC.business_id = ${req.body.business_id}
+				AND BC.customer_id = C.id AND BC.active=1`;
+
+
+    connection.query(query, function(err, rows, fields) {
+        if (!err) {
+
+            //get day of week
+            var today = weekdays[new Date().getDay()];
+            //for each subscribed customer, send SMS if applicable
+            for (let customer of rows) {
+                //if customer has not opted for alerts today, skip
+                if (customer[today] == 0) break;
+                //format the customer phone number with country code
+                var phone = `+${customer.country_code}${customer.mobile}`;
+                //notify the customer
+                console.log(`Attempting to send SMS to ${phone}`);
+                notifyCustomer(phone, req.body.text);
+            }
+            res.json('success');
+        } else {
+            console.log(err)
+            res.json(err)
+        }
+    });
+
+    connection.end();
+
+});
+
+
+app.listen(3000);
+
+console.log("Express app running on port 3000");
+
+module.exports = app;
+
+
+
+function notifyCustomer(phone, msg) {
+    // Twilio Credentials
+    var accountSid = '<accountSid>';
+    var authToken = '<authToken>';
+    //require the Twilio module and create a REST client
+    var client = require('twilio')(accountSid, authToken);
+    client.messages.create({
+        to: phone,
+        from: "+<number>",
+        body: msg
+    }, function(err, message) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log(`Message sent: ${message.sid}`);
+        }
+
+    });
+
+}
