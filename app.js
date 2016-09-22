33812c84d4fbe413704d3886b7c0f35e4157e41e
Waqid <wmv214@nyu.edu>
2016-09-22 17:22:30 -0400

    Added toaster message

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ app.use(function(req, res, next) {
 app.use(express.static("./public"));
 app.use('/jquery', express.static(__dirname + '/node_modules/jquery/dist/'));
 app.use('/bootstrap', express.static(__dirname + '/node_modules/bootstrap/dist/'));
+app.use('/lib', express.static(__dirname + '/lib/'));
 
 
 app.get("/api/owner/:id", function(req, res) {
@@ -59,20 +60,24 @@ app.post("/api/notify", function(req, res) {
 
     connection.query(query, function(err, rows, fields) {
         if (!err) {
-
+            let count = 0;
             //get day of week
             const today = weekdays[new Date().getDay()];
             //for each subscribed customer, send SMS if applicable
             for (let customer of rows) {
                 //if customer has not opted for alerts today, skip
                 if (customer[today] == 0) break;
+                count++;
                 //format the customer phone number with country code
                 const phone = `+${customer.country_code}${customer.mobile}`;
                 //notify the customer
                 console.log(`Attempting to send SMS to ${phone}`);
                 notifyCustomer(phone, req.body.text);
             }
-            res.json('success');
+            let msg = (count > 0 ) ?
+                      (count > 1 ) ? `${count} customers have been notified.`: `${count} customer has been notified.`
+                      : `There are 0 customers subscribed for your alerts today.`
+            res.json({msg: msg});
         } else {
             console.log(err)
             res.json(err)
@@ -94,13 +99,13 @@ module.exports = app;
 
 function notifyCustomer(phone, msg) {
     // Twilio Credentials
-    const accountSid = '<accountSid>';
-    const authToken = '<authToken>';
+    const accountSid = '<add-accountSid>';
+    const authToken = '<add-authToken>';
     //require the Twilio module and create a REST client
     const client = require('twilio')(accountSid, authToken);
     client.messages.create({
         to: phone,
-        from: "+<number>",
+        from: "<add-phone>",
         body: msg
     }, function(err, message) {
         if (err) {
